1b4a283e1f1735e310c14f537dd9b024d0907a8f
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2023-03-07 00:07:36 -0300

    index/routes

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config/database.php';
+
+use FastRoute\RouteCollector;
+
+// Instancia o objeto da classe RouteCollector do FastRoute
+$dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
+    // Define as rotas para cada endpoint da API
+    $r->addRoute('GET', '/api/products', 'ProductController@index');
+    $r->addRoute('GET', '/api/products/{id:\d+}', 'ProductController@show');
+    $r->addRoute('POST', '/api/products', 'ProductController@store');
+    $r->addRoute('PUT', '/api/products/{id:\d+}', 'ProductController@update');
+    $r->addRoute('DELETE', '/api/products/{id:\d+}', 'ProductController@destroy');
+
+    $r->addRoute('GET', '/api/categories', 'CategoryController@index');
+    $r->addRoute('GET', '/api/categories/{id:\d+}', 'CategoryController@show');
+    $r->addRoute('POST', '/api/categories', 'CategoryController@store');
+    $r->addRoute('PUT', '/api/categories/{id:\d+}', 'CategoryController@update');
+    $r->addRoute('DELETE', '/api/categories/{id:\d+}', 'CategoryController@destroy');
+
+    $r->addRoute('GET', '/api/taxes', 'TaxController@index');
+    $r->addRoute('GET', '/api/taxes/{id:\d+}', 'TaxController@show');
+    $r->addRoute('POST', '/api/taxes', 'TaxController@store');
+    $r->addRoute('PUT', '/api/taxes/{id:\d+}', 'TaxController@update');
+    $r->addRoute('DELETE', '/api/taxes/{id:\d+}', 'TaxController@destroy');
+});
+
+// Define a URL atual a ser processada
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Remove a query string e os caracteres finais "/" da URI, se houverem
+if (($pos = strpos($uri, '?')) !== false) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rtrim($uri, '/');
+
+// Processa a rota e chama o respectivo controller e método, se encontrados
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // Página não encontrada
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        echo '404 Not Found';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        // Método HTTP não permitido para essa rota
+        header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed");
+        echo '405 Method Not Allowed';
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        // Rota encontrada: chama o controller e método correspondentes
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        [$controllerName, $methodName] = explode('@', $handler);
+        $controllerClass = '\\Controllers\\' . $controllerName;
+        $controller = new $controllerClass();
+        $controller->$methodName($vars);
+        break;
+}
