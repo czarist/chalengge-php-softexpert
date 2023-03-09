4b0195c2e568015b5404b91cb908cd62fcbbb687
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2023-03-09 05:47:26 -0300

    database configs

--- config/database.php
+++ config/database.php
@@ -2,7 +2,6 @@
 
 namespace config;
 
-use Dotenv\Dotenv;
 use PDOException;
 use PDO;
 
@@ -13,14 +12,11 @@ class Database
 
     private function __construct()
     {
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-        $dotenv->load();
-
-        $host = $_ENV['DB_HOST'];
-        $port = $_ENV['DB_PORT'];
-        $dbname = $_ENV['DB_NAME'];
-        $user = $_ENV['DB_USER'];
-        $password = $_ENV['DB_PASS'];
+        $host = DB_HOST;
+        $port = DB_PORT;
+        $dbname = DB_NAME;
+        $user = DB_USER;
+        $password = DB_PASS;
 
         try {
             $dsn = "pgsql:host={$host};port={$port};dbname={$dbname}";
--- config/migrate.php
+++ config/migrate.php
@@ -1,14 +1,23 @@
 <?php
 
-require_once 'database.php';
+$host = DB_HOST;
+$port = DB_PORT;
+$dbname = DB_NAME;
+$user = DB_USER;
+$password = DB_PASS;
 
-use config\Database;
+$dsn = "pgsql:host=$host;port={$port};dbname=$dbname;user=$user;password=$password";
 
-$pdo = Database::getInstance()->getConnection();
+$dbconn = new PDO($dsn);
 
 $sql = file_get_contents('migrate.sql');
 
-// Executa a query
-$pdo->exec($sql);
+$result = $dbconn->exec($sql);
 
-echo "Arquivo SQL executado com sucesso!";
+if ($result === false) {
+    echo "An error occurred while executing the SQL.: " . $dbconn->errorInfo()[2];
+} else {
+    echo "SQL executed successfully!";
+}
+
+$dbconn = null;
