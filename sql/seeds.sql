9fcbba178556375cff44a5b9d050c2d82064e4e2
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2023-03-11 15:41:09 -0300

    sql ajuste

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -293,7 +293,7 @@ VALUES (
         'John Doe',
         '12345678901',
         '2023-03-07 14:30:00',
-        '[{"item": "Product 1", "quantity": 2, "price": 10.50}, {"item": "Product 2", "quantity": 1, "price": 7.90}]',
+        '[{"item": "Product 1", "item_id": 1, "quantity": 2, "price": 10.50, "item_tax_id": 2}, {"item": "Product 2", "item_id": 2, "quantity": 1, "price": 7.90, "item_tax_id": 1}]',
         28.90
     );
 INSERT INTO sales (
@@ -307,7 +307,7 @@ VALUES (
         'Jane Smith',
         '09876543210',
         '2023-03-07 15:00:00',
-        '[{"item": "Product 3", "quantity": 1, "price": 15.75}]',
+        '[{"item": "Product 3", "item_id": 1, "quantity": 1, "price": 15.75, "item_tax_id": 2}]',
         15.75
     );
 INSERT INTO sales (
@@ -321,7 +321,7 @@ VALUES (
         'Bob Johnson',
         '45678901234',
         '2023-03-07 16:30:00',
-        '[{"item": "Product 4", "quantity": 3, "price": 5.00}]',
+        '[{"item": "Product 4", "item_id": 1, "quantity": 3, "price": 5.00, "item_tax_id": 3}]',
         15.00
     );
 INSERT INTO sales (
@@ -335,7 +335,7 @@ VALUES (
         'Maria Silva',
         '98765432100',
         '2023-03-07 17:15:00',
-        '[{"item": "Product 2", "quantity": 2, "price": 7.90}, {"item": "Product 5", "quantity": 1, "price": 25.00}]',
+        '[{"item": "Product 2", "item_id": 1, "quantity": 2, "price": 7.90, "item_tax_id": 2}, {"item": "Product 5", "item_id": 2, "quantity": 1, "price": 25.00, "item_tax_id": 3}]',
         40.80
     );
 INSERT INTO sales (
@@ -349,34 +349,6 @@ VALUES (
         'Joe Black',
         '33333333333',
         '2023-03-07 18:00:00',
-        '[{"item": "Product 1", "quantity": 4, "price": 10.50}]',
+        '[{"item": "Product 1", "item_id": 1, "quantity": 4, "price": 10.50, "item_tax_id": 1}]',
         42.00
-    );
-INSERT INTO sales (
-        buyer_name,
-        buyer_cpf,
-        purchase_date,
-        items_list,
-        total_value
-    )
-VALUES (
-        'Julia Garcia',
-        '22222222222',
-        '2023-03-07 19:45:00',
-        '[{"item": "Product 6", "quantity": 2, "price": 8.00}, {"item": "Product 7", "quantity": 1, "price": 12.50}]',
-        28.50
-    );
-INSERT INTO sales (
-        buyer_name,
-        buyer_cpf,
-        purchase_date,
-        items_list,
-        total_value
-    )
-VALUES (
-        'Carlos Oliveira',
-        '77777777777',
-        '2023-03-07 20:30:00',
-        '[{"item": "Product 8", "quantity": 1, "price": 18.99}, {"item": "Product 9", "quantity": 1, "price": 7.00}]',
-        25.99
     );
